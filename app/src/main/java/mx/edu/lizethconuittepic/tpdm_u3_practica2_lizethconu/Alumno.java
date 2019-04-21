928478203b0dc66f94ce20dd8ecea27fa7a0a65c
edwin97e <edwin97e@gmail.com>
2019-04-20 21:49:21 -0600

    uno

---
+++ app/src/main/java/mx/edu/lizethconuittepic/tpdm_u3_practica2_lizethconu/Alumno.java
@@ -0,0 +1,12 @@
+package mx.edu.lizethconuittepic.tpdm_u3_practica2_lizethconu;
+
+public class Alumno {
+    String nombre, domicilio, edad, telefono;
+
+    public Alumno(String nom, String dom, String ed, String tel){
+        nombre = nom;
+        domicilio = dom;
+        edad = ed;
+        telefono = tel;
+    }
+}
---
+++ app/src/main/java/mx/edu/lizethconuittepic/tpdm_u3_practica2_lizethconu/Docente.java
@@ -0,0 +1,12 @@
+package mx.edu.lizethconuittepic.tpdm_u3_practica2_lizethconu;
+
+public class Docente {
+    String nombre, rfc, domicilio, telefono;
+
+    public Docente(String nombre, String domicilio, String rfc, String telefono) {
+        this.nombre = nombre;
+        this.rfc = rfc;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+    }
+}
